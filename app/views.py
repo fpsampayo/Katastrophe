abb0cd3966b09007f16614af0dde3dc8155ad082
Francisco Pérez Sampayo <fpsampayo@gmail.com>
2018-02-14 16:33:41 +0100

    Pequeñas modificaciones

--- app/views.py
+++ app/views.py
@@ -2,13 +2,15 @@
 from app import app
 from flask import request, jsonify
 import requests
+from osgeo import ogr, gdal, osr
 from xml.etree import ElementTree as ET
+import json
 
 @app.route('/')
 def hello_world():
     return '''
         <h4>Acceso a los servicios web del catastro.</h4>
-        <a href="http://katastroph.herokuapp.com/coor?srs=EPSG:4326&x=-8.588562011718752&y=42.28137302193453">
+        <a href="http://katastrophe.herokuapp.com/coor?srs=EPSG:4326&x=-8.588562011718752&y=42.28137302193453">
         Ejemplo de petición por coordenadas</a>
     '''
 
@@ -58,7 +60,6 @@ def coor():
         root = ET.fromstring(response.text.encode('utf-8'))
         err = root.find('*//c:err/c:des', ns)
         if err != None:
-            print "Error"
             return handler500(err.text)
         pc1 = root.find('*//c:pc1', ns).text
         pc2 = root.find('*//c:pc2', ns).text
@@ -75,6 +76,53 @@ def coor():
              'parcela': parc,
              'accesoSede': urlAccesoSede}
 
-        return jsonify(r)
+        response = jsonify(r)
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
     else:
         return handler500("Ocurrio un problema conectando con Catastro.")
+
+
+@app.route('/parcel')
+def cadastralParcel():
+
+    refcat = request.args.get('refcat')
+
+    url = 'http://ovc.catastro.meh.es/INSPIRE/wfsCP.aspx?service=wfs&version=2&request=getfeature&STOREDQUERIE_ID=GetParcel&srsname=EPSG:4326&REFCAT=%s'
+    
+    wfs_drv = ogr.GetDriverByName('WFS')
+    wfs_ds = wfs_drv.Open('WFS:' + url % refcat)
+
+    try:
+        layer = wfs_ds.GetLayerByName('cp:CadastralParcel')
+        feat = layer.GetFeature(0)
+        geom = feat.GetGeometryRef()
+
+        # Realizamos transformación para invertir los ejes
+        source = osr.SpatialReference()
+        source.ImportFromProj4('+proj=latlong +datum=WGS84 +axis=neu +wktext')
+        
+        target = osr.SpatialReference()
+        target.ImportFromProj4('+proj=latlong +datum=WGS84 +axis=enu +wktext')
+
+        transform = osr.CoordinateTransformation(source, target)
+
+        geom.Transform(transform)
+
+        area = feat['areaValue']
+        geomJson = geom.ExportToJson()
+
+        j = {
+            'type': 'Feature',
+            'properties': {
+                'refcat': refcat,
+                'area': area
+            },
+            'geometry': json.loads(geomJson)
+        }
+
+        response = jsonify(j)
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+    except:
+        return handler500("Error conectando a catastro")
